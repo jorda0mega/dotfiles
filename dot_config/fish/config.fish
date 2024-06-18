e4329f8f7c3397408fe1eedba787c6f23840549c
Jorge David <484084+jorda0mega@users.noreply.github.com>
2024-06-18 16:12:43 -0400

    organize starship themes change terminal theme to pastel-powerline

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -1,9 +1,9 @@
 # Starship config file loaded based on terminal
 starship init fish | source
 if test "$TERM_PROGRAM" != "WarpTerminal"
-  set -x STARSHIP_CONFIG "$HOME/.config/starship/starship.toml" # for any terminal
+  set -x STARSHIP_CONFIG "$HOME/.config/starship/pastel-powerline/starship.toml" # for any terminal
 else
-  set -x STARSHIP_CONFIG "$HOME/.config/starship/starship_warp.toml" # for Warp
+  set -x STARSHIP_CONFIG "$HOME/.config/starship/pastel-powerline/starship_warp.toml" # for Warp
 end
 set -gx VOLTA_HOME "$HOME/.volta"
 set -gx PATH "$VOLTA_HOME/bin" $PATH
