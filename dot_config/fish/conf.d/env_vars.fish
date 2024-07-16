e682f442044c55fa186738bdddf62bcccb0561a2
Jorge David <484084+jorda0mega@users.noreply.github.com>
2024-07-16 15:05:12 -0400

    Update .config/fish/conf.d/env_vars.fish

--- dot_config/fish/conf.d/env_vars.fish
+++ dot_config/fish/conf.d/env_vars.fish
@@ -1,7 +1,7 @@
 if test "$TERM_PROGRAM" != "WarpTerminal"
   set -x STARSHIP_CONFIG "$HOME/.config/starship/default/starship.toml" # for any terminal
 else
-  set -x STARSHIP_CONFIG "$HOME/.config/starship/default/starship.toml" # for Warp
+  set -x STARSHIP_CONFIG "$HOME/.config/starship/default/starship_warp.toml" # for Warp
 end
 set -gx VOLTA_HOME "$HOME/.volta"
 set -gx PATH "$VOLTA_HOME/bin" $PATH
