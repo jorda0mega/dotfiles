9b779b44fc2a2b99c35699f1be4dc94eb2ab0098
Jorge David <484084+jorda0mega@users.noreply.github.com>
2024-07-16 13:23:11 -0400

    Update .config/fish/config.fish Change attributes of .config/fish/conf.d/env_vars.fish

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -1,10 +1,2 @@
 # Starship config file loaded based on terminal
 starship init fish | source
-if test "$TERM_PROGRAM" != "WarpTerminal"
-  set -x STARSHIP_CONFIG "$HOME/.config/starship/pastel-powerline/starship.toml" # for any terminal
-else
-  set -x STARSHIP_CONFIG "$HOME/.config/starship/pastel-powerline/starship_warp.toml" # for Warp
-end
-set -gx VOLTA_HOME "$HOME/.volta"
-set -gx PATH "$VOLTA_HOME/bin" $PATH
-set -x VOLTA_FEATURE_PNPM 1
