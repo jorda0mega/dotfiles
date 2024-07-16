45f2b38533f037babdfc602d4e8343bdf289babf
Jorge David <484084+jorda0mega@users.noreply.github.com>
2024-07-16 13:53:03 -0400

    Add .config/fish/conf.d/env_vars.fish

---
+++ dot_config/fish/conf.d/env_vars.fish
@@ -0,0 +1,8 @@
+if test "$TERM_PROGRAM" != "WarpTerminal"
+  set -x STARSHIP_CONFIG "$HOME/.config/starship/default/starship.toml" # for any terminal
+else
+  set -x STARSHIP_CONFIG "$HOME/.config/starship/default/starship.toml" # for Warp
+end
+set -gx VOLTA_HOME "$HOME/.volta"
+set -gx PATH "$VOLTA_HOME/bin" $PATH
+set -x VOLTA_FEATURE_PNPM 1
